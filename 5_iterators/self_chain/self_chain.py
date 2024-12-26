96b0572c7b0e545042858af0921f326158bab899
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2024-12-26 16:31:18 +0300

    Update self_chain.py

--- 5_iterators/self_chain/self_chain.py
+++ 5_iterators/self_chain/self_chain.py
@@ -1,12 +1,25 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Iterator
 
 T = TypeVar("T")
 
-
 def chain(*iterables: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь"""
-
+    """Соединяет несколько iterable в один."""
+    for iterable in iterables:
+        for item in iterable:
+            yield item
 
 class Chain:
     def __init__(self, *iterables: Iterable[T]):
-        """Реализуйте класс ниже"""
+        self.iterables = iterables
+        self.current_iter = iter(iterables)
+        self.current_iter_instance = iter(next(self.current_iter))
+
+    def __iter__(self) -> 'Chain':
+        return self
+
+    def __next__(self) -> T:
+        while True:
+            try:
+                return next(self.current_iter_instance)
+            except StopIteration:
+                self.current_iter_instance = iter(next(self.current_iter))
