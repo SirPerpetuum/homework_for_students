0488bd1cab6be7e16547b23137fab060685aa9f6
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2024-12-26 16:22:23 +0300

    Update retry.py

--- 4_decorators/retry/retry.py
+++ 4_decorators/retry/retry.py
@@ -1,2 +1,24 @@
-# реализуйте декоратор вида @retry(count: int, delay: timedelta, handled_exceptions: tuple[type(Exceptions)])
+import time
+from datetime import timedelta
+from functools import wraps
 
+
+def retry(count=3, delay=timedelta(seconds=1), handled_exceptions=(Exception,)):
+    if count < 1:
+        raise ValueError("Параметр count должен быть не менее 1.")
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(count):
+                try:
+                    return func(*args, **kwargs)
+                except handled_exceptions as e:
+                    if attempt < count - 1:  # Проверяем, это ли последняя попытка
+                        time.sleep(delay.total_seconds())
+                    else:
+                        raise e  # Повторно выбрасываем последнее исключение
+
+        return wrapper
+
+    return decorator
