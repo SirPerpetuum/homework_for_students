363d2673f6fe6a351749b0c5eb9914b12c0bfe70
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2024-12-26 16:32:38 +0300

    Update self_cycle.py

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -1,12 +1,23 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Iterator
 
 T = TypeVar("T")
 
-
-def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь."""
-
-
+def cycle(iterable: Iterable[T]) -> Generator[T, None, None]:
+    """Бесконечный цикл по элементам iterable."""
+    while True:
+        for item in iterable:
+            yield item
 class Cycle:
-    def __init__(self, obj: Iterable[T]):
-        """Реализуйте класс"""
+    def __init__(self, iterable: Iterable[T]):
+        self.iterable = iterable
+        self.iterator = iter(iterable)
+
+    def __iter__(self) -> 'Cycle':
+        return self
+
+    def __next__(self) -> T:
+        while True:
+            try:
+                return next(self.iterator)
+            except StopIteration:
+                self.iterator = iter(self.iterable)
