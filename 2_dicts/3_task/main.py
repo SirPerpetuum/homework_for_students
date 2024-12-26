c1a1295389c445735b1e4cb9e4a1d907f91980e3
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2024-12-26 16:14:01 +0300

    Update main.py

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -7,6 +7,20 @@ def format_phone(phone_number: str) -> str:
     Returns:
         отформатированный телефон
     """
-    formatted_phone_number = ""
+    cleaned_number = phone_number
+    for digit in phone_number:
+        if digit not in "0123456789":
+            cleaned_number = cleaned_number.replace(digit, "")
 
-    return formatted_phone_number
+    if len(cleaned_number) == 11:
+        formatted_number = "8 (9" + cleaned_number[2] + cleaned_number[3] + ") " + cleaned_number[4] + cleaned_number[
+            5] + cleaned_number[6] + "-" + \
+                           cleaned_number[7] + cleaned_number[8] + "-" + cleaned_number[9] + cleaned_number[10]
+    elif len(cleaned_number) == 10:
+        formatted_number = "8 (9" + cleaned_number[1] + cleaned_number[2] + ") " + cleaned_number[3] + cleaned_number[
+            4] + cleaned_number[5] + "-" + \
+                           cleaned_number[6] + cleaned_number[7] + "-" + cleaned_number[8] + cleaned_number[9]
+    else:
+        formatted_number = cleaned_number
+
+    return formatted_number
