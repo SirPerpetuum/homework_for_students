42060fb9f6b52771b332c7b7479618789cace94c
SirPerpetuum <84527074+SirPerpetuum@users.noreply.github.com>
2024-12-26 15:55:22 +0300

    Update main.py

--- 1_strings/3_maximum_number_of_words/main.py
+++ 1_strings/3_maximum_number_of_words/main.py
@@ -11,4 +11,13 @@ pytest ./3_maximum_number_of_words/test.py
 
 def get_max_number_of_words_from_sentences(sentences: list[str]) -> int:
     """Пишите ваш код здесь."""
-
+    i = 1
+    max_i = 0
+    for sentenc in sentences:
+        for word in sentenc:
+            if word == ' ':
+                i += 1
+            if max_i < i:
+                max_i = i
+        i = 1
+    return max_i
